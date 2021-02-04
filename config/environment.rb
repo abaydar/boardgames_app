6b8bc06bd2b93b1d9a94a747ba2ed4bfefbf8183
+ <+>
2021-02-04 14:15:51 -0500

    configure environment with activerecord

---
+++ config/environment.rb
@@ -0,0 +1,13 @@
+# load dependencies/gemfile
+require 'bundler/setup'
+Bundler.require
+
+
+require_relative '../app/boardgame'
+
+# establish connection to db
+
+ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3'
+    :database => 'db/boardgames.sqlite'
+)
