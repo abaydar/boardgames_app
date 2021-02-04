cf7169bcfbac7bae3c13656d2d453990252f0780
+ <+>
2021-02-04 14:23:44 -0500

    configure rakefile

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,6 @@ require_relative '../app/boardgame'
 # establish connection to db
 
 ActiveRecord::Base.establish_connection(
-    :adapter => 'sqlite3'
+    :adapter => 'sqlite3',
     :database => 'db/boardgames.sqlite'
 )
