5db64332d311cc0eb91277130edf32fe011e4629
+ <+>
2021-02-10 16:53:35 -0500

    added show view and updated controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,16 @@ class ApplicationController < Sinatra::Base
         set :views, "app/views"
         #set :public_dir, "public"
     end
-  
-    get "/" do
+
+    get '/boardgames/' do
+        @boardgames = Boardgame.all
         erb :index
     end
+
+
+    get '/boardgames/:id' do
+        @boardgame = Boardgame.find(params[:id])
+        erb :show
+    end
+
   end
